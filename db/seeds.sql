b4d1224e07d51fd744597eff4e3fba0f29873a5f
Nicole Remy <nicoleremy95@gmail.com>
2020-07-20 09:59:11 -0700

    created files and file struture template, npm init express express handlebars mysql, finished seeds and schema files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger, devoured) VALUES ('Big Blue');
+INSERT INTO burgers (burger, devoured) VALUES ('All American', true);
+INSERT INTO burgers (burger, devoured) VALUES ('Classic', true);
